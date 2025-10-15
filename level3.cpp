10d40923362e8e4fbeec529f60728e70075cc4a8
Razoryu <ranggarassya22@gmail.com>
2025-10-15 11:06:26 +0700

    Add files via upload

--- level3.cpp
+++ level3.cpp
@@ -1,22 +1,22 @@
+/*
+
+*/
+
 #include <iostream>
 using namespace std;
 
-int main(){
-    int nums[]{4,3,2,9};
-    int panjang = sizeof(nums)/sizeof(nums[0]);
+int nums[] = {0,1,0,3,12};
+int panjang = sizeof(nums)/sizeof(nums[0]);
 
-    cout << "[";
+int main(){
     for(int i = 0; i < panjang; i++){
-        if(i == panjang - 1){
-            nums[i] = nums[i] + 1;
+        if(nums[i] != 0){
+            cout << nums [i] << " ";
         }
-        if(nums[i] == 10){
-            cout << "1 0";
-        }
-        else {
-            cout << nums[i] << " ";
-        }
-        
     }
-    cout << "]";
+    for(int i = 0; i < panjang; i++){
+        if(nums[i] == 0){
+            cout << nums [i] << " ";
+        }
+    }
 }
